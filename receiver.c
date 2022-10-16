e762959573f60a7a72a2e83821a6e14c9fa1a9ce
Ishan Phansalkar <52034505+ishanph@users.noreply.github.com>
2022-10-16 18:38:15 +0530

    Added comments

--- receiver.c
+++ receiver.c
@@ -9,25 +9,25 @@
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
     
-#define PORT1 8080
-#define PORT2 8082
+#define PORT 8080
 #define MAXLINE 1024 
     
 // Driver code 
 int main(int argc, char* argv[]) { 
-    if(argc != 4)
+    if(argc != 4) //check for correct usage
         return -1;
-
+    
+    // Storing user input
     int rec_port = atoi(argv[1]);
     int sen_port = atoi(argv[2]);
     float p_drop = atof(argv[3]);
 
+    // Initialising the sockets
     int sockfd; 
     char buffer[MAXLINE]; 
-    char *hello = "Hello from the other side";
     struct sockaddr_in servaddr, cliaddr; 
         
-    // Creating socket file descriptor 
+    // Creating a socket file descriptor 
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
         perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
@@ -36,50 +36,51 @@ int main(int argc, char* argv[]) {
     memset(&servaddr, 0, sizeof(servaddr)); 
     memset(&cliaddr, 0, sizeof(cliaddr));
         
-    // Filling server information 
+    // Adding server information 
     servaddr.sin_family = AF_INET; // IPv4 
     servaddr.sin_addr.s_addr = INADDR_ANY; 
-    servaddr.sin_port = htons(PORT1); 
+    servaddr.sin_port = htons(PORT); 
         
-    // Bind the socket with the server address 
+    // Binding the socket with the server address 
     if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
-            sizeof(servaddr)) < 0 ) 
+            sizeof(servaddr)) < 0)
     { 
         perror("bind failed"); 
         exit(EXIT_FAILURE); 
     }
 
-    int len, n; 
-    int next_num = 1;
-    int received_num;
+    // Variable initialisation
+    int len, n, received_num; 
     bool drop;
     float random;
+    int next_num = 1;
     len = sizeof(cliaddr);  //len is value/result 
-    srand((unsigned int) time(NULL));
+    srand((unsigned int) time(NULL)); //initialise the random number generator
     
+    // Infinite reception loop
     while(1){
-        n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE,
                     MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
                     &len);
         buffer[n] = '\0';
 
         char* msg;
-        sscanf(buffer, "%*[^0123456789]%d", &received_num);
+        sscanf(buffer, "%*[^0123456789]%d", &received_num); //extract the recieved package
         
         drop = 0;
-        if(received_num == next_num)
+        if(received_num == next_num) //if the correct packet is recieved, generate a random number
             random = ((float) rand() / (float)(RAND_MAX));
         else
             random = 1;
 
-        if(random > p_drop){
-            if(received_num == next_num)
+        if(random > p_drop){ //send and ack if the packet isn't dropped, go back to the loop otherwise
+            if(received_num == next_num) //if the packet was correct, increment the ack number
                 next_num++;
 
-            asprintf(&msg, "Acknowledgement:%d\n", next_num);
+            asprintf(&msg, "Acknowledgement:%d\n", next_num); //generate the ack
 
             printf("Sender : %s\n", buffer);
-            sendto(sockfd, (char *)msg, strlen(msg),  
+            sendto(sockfd, (char *)msg, strlen(msg),  //send the ack
                 2048, (const struct sockaddr *) &cliaddr, 
                     len); 
 
--- sender.c
+++ sender.c
@@ -14,84 +14,86 @@
 #define PORT 8080
 #define MAXLINE 1024
 	
-// Driver code
 int main(int argc, char* argv[]) {
-	if(argc != 5)
+	if(argc != 5) //check for correct usage
     return -1;
 
+	// Storing user input
     int rec_port = atoi(argv[1]);
     int sen_port = atoi(argv[2]);
     float rtt = atof(argv[3]);
 	int p = atoi(argv[4]);
 
+	// Initialising the sockets
 	int sockfd;
 	char buffer[MAXLINE];
-	char *hello = "Hello from sender";
 	struct sockaddr_in	 servaddr;
 	clock_t start, end;
     double cpu_time_used;
 	
-	// Creating socket file descriptor
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+	// Creating a socket file descriptor
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
 	
 	memset(&servaddr, 0, sizeof(servaddr));
 		
-	// Filling server information
+	// Adding server information
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(PORT);
 	servaddr.sin_addr.s_addr = INADDR_ANY;
 		
-	int n, len;
-	int num = 0;
+	// Variable initialisation
+	int n, len, received_num;
 	char* msg;
+	int num = 0;
 	bool lost = 0;
-	int num_rec;
 
+	// Loop for sending all the packets
 	while(num < p){
 		lost = 1;
-		num++;
-		asprintf(&msg, "Packet:%d\n", num);
+		num++; //increment the next packet number
+		asprintf(&msg, "Packet:%d\n", num); //creating the message
 		printf("%s\n", msg);
 
-		sendto(sockfd, (char *)msg, strlen(msg),
+		sendto(sockfd, (char *)msg, strlen(msg), //sending the message to the receiver
 			2048, (const struct sockaddr *) &servaddr,
 				sizeof(servaddr));
 
 		printf("Message sent.\n");
 				
-		start = clock();
+		start = clock(); //start the timer for the wait time
+
+		// Polling loop to recive the acks
 		while(1){
-			n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+			n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
 						MSG_DONTWAIT, (struct sockaddr *) &servaddr,
 						&len);
 
 
 			if(n > 0){
-				int received_num;
-        		sscanf(buffer, "%*[^0123456789]%d", &received_num);
-				if(received_num == num + 1){ //get the seg number from the message
+        		sscanf(buffer, "%*[^0123456789]%d", &received_num); //extract the packet number from the message
+				if(received_num == num + 1){ //check if the ack is correct
 					lost = 0;
 					printf("Successful tx\n");
 				}
 				else{
-					lost = 1;
+					lost = 1; //consider packet lost if the ack is incorrect
 				}
 				break;
 			}
 
 			end = clock();
-			if((( (double) (end - start)) / CLOCKS_PER_SEC) > rtt){
-				lost = 1;
+			if((((double) (end - start)) / CLOCKS_PER_SEC) > rtt){ //check for timeout 
+				lost = 1; //consider the packet lost if an ack isn't received
 				break;
 			}
 
 		}
 
 		if(lost){
-			num--;
+			num--; //reset num since the packet wasn't received
 			printf("Packet lost\n");
 			continue;
 		}
@@ -99,6 +101,6 @@ int main(int argc, char* argv[]) {
 		buffer[n] = '\0';
 		printf("Receiver : %s\n", buffer);
 	}
-	close(sockfd);
+	close(sockfd); //close the socket
 	return 0;
 }
